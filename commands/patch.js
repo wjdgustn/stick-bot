b58b7f5b072b29b123f7f55aeb288964360e9454
Rhythmfactory <64122960+junhee6296@users.noreply.github.com>
2021-10-15 13:35:09 +0900

    Update patch.js

--- commands/patch.js
+++ commands/patch.js
@@ -1,2 +1,12 @@
-
-content: '대충 메시지'
+module.exports = {
+    info: {
+        name: 'patch',
+        description: '패치노트를 확인합니다. ',
+    },
+    handler: async interaction => {
+        return interaction.reply({
+            content: 'https://docs.google.com/document/d/1eF7eRCoFbkDGTpRbx-ZvsAWTatkRsIfXJVKmqRCi_eI/edit?usp=sharing',
+            ephemeral: true
+        });
+    }
+}
