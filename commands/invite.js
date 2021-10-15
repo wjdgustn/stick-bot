f270e4d9681743bbcc5aee9e8955d8bff8f4124c
Rhythmfactory <64122960+junhee6296@users.noreply.github.com>
2021-10-15 16:57:12 +0900

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,15 +1,12 @@
-const setting = require('../setting.json');
-
 module.exports = {
     info: {
-        name: 'invite',
-        description: '봇을 초대하는 링크를 표시합니다. // Show bot invite link.'
+        name: 'patch',
+        description: `패치노트를 확인합니다. / Check Bot's patch note`,
     },
     handler: async interaction => {
-        return interaction.reply(
-            lang.langByLangName(interaction.dbUser.lang, 'BOT_INVITE')
-                .replace('{minperm}', `https://discord.com/api/oauth2/authorize?client_id=${interaction.client.user.id}&permissions=${setting.BOT_PERMISSION}&scope=bot%20applications.commands`)
-                .replace('{admin}', `https://discord.com/api/oauth2/authorize?client_id=${interaction.client.user.id}&permissions=8&scope=bot%20applications.commands`)
-        );
+        return interaction.reply({
+            content: `메시지 관리권한만 / Only manage message : https://discord.com/api/oauth2/authorize?client_id=${interaction.client.user.id}&permissions=8192&scope=bot%20applications.commands\n모든 권한 (어드민) / Full Permission (ADMIN) : https://discord.com/api/oauth2/authorize?client_id=${interaction.client.user.id}&permissions=8&scope=bot%20applications.commands`,
+            ephemeral: true
+        });
     }
-}
+};
