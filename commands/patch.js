775019b5124c0ff949feb91833c6c4cd8c1ab498
Rhythmfactory <64122960+junhee6296@users.noreply.github.com>
2021-12-08 15:10:49 +0900

    Update patch.js

--- commands/patch.js
+++ commands/patch.js
@@ -1,7 +1,7 @@
 module.exports = {
     info: {
         name: 'patch',
-        description: '패치노트를 확인합니다. ',
+        description: `패치노트를 확인합니다. / check bot's patch note`,
     },
     handler: async interaction => {
         return interaction.reply({
