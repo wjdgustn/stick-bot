c458b3444201c8b461e481d0f5d0a80fac936dda
Rhythmfactory <64122960+junhee6296@users.noreply.github.com>
2021-12-08 15:16:55 +0900

    Create support.js

---
+++ commands/support.js
@@ -0,0 +1,12 @@
+module.exports = {
+    info: {
+        name: 'suport',
+        description: `지원 서버 링크를 확인합니다. / Check bot's Support server Link.`,
+    },
+    handler: async interaction => {
+        return interaction.reply({
+            content: `지원 서버 링크 / Support Server invite link : https://discord.gg/zESCFncp28`,
+            ephemeral: true
+        });
+    }
+};
